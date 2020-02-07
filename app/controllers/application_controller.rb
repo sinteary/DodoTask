9cf6da382dcdaae14685460629e708b21547e326
sinteary <leewengyancynthia@gmail.com>
2020-02-07 23:48:17 +0800

    Add headers and css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
   
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, PATCH, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
+
   def login!
     session[:user_id] = @user.id
   end
